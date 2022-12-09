3f9e7d95b6b9deb32e8652453795dd8372bfa292
Abnish <89826336+abnishnayak@users.noreply.github.com>
2022-12-09 11:11:35 +0530

    Update lms_db.sql

--- lms_db.sql
+++ lms_db.sql
@@ -1,25 +1,27 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `lms_db`(
-in user_id int,
-in username varchar(40), 
-in email varchar(40), 
-in contact int, 
-in pwd varchar(20), 
-in address varchar(70), 
-in pincode int
+in _user_id int,
+in _username varchar(40), 
+in _email varchar(40), 
+in _contact int, 
+in _pwd varchar(20), 
+in _address varchar(70), 
+in _pincode int
 )
 BEGIN
-if user_id is null then
+if _user_id is null then
 insert into user_details(username,email,contact,pwd,address,pincode)
-values (username,email,contact,pwd,address,pincode);
-select last_insert_id() as user_id;
+values (_username,_email,_contact,_pwd,_address,_pincode);
+select _user_id = last_insert_id();
 else 
 update user_details
 set
-username=username,
-email=email,
-contact=contact,
-pwd=pwd,
-address=address,
-pincode=pincode;
+username=_username,
+email=_email,
+contact=_contact,
+pwd=_pwd,
+address=_address,
+pincode=_pincode
+where user_id = _user_id;
 end if;
+select _user_id as 'user_id';
 END
